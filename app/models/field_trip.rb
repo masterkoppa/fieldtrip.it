742bcd55c3076ecc8066420a7480ca50fd42f6df
Andres Ruiz <andresruiz2010@gmail.com>
2014-06-15 15:24:41 -0700

    Merge the file changes back

--- app/models/field_trip.rb
+++ app/models/field_trip.rb
@@ -6,7 +6,4 @@ class FieldTrip < ActiveRecord::Base
   has_many :employees, through: :employees_field_trips
   accepts_nested_attributes_for :address
 
-  def attending?(employee)
-    employee.in?self[:employees]
-  end
 end
