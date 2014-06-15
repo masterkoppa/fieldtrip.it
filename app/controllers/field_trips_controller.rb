e89f09a3020780018d3f43827ff51a8975a26765
Andres Ruiz <andresruiz2010@gmail.com>
2014-06-15 15:42:05 -0700

    Initial draft of algorithm for sorting people into buckets
    Doesn't take into account preferences

--- app/controllers/field_trips_controller.rb
+++ app/controllers/field_trips_controller.rb
@@ -73,4 +73,60 @@ class FieldTripsController < ApplicationController
     def field_trip_params
       params.require(:field_trip).permit(:name, :description, :start_time, :end_time, :company_id)
     end
+
+    def sort_employees_no_preference
+
+      employee_list = [{name: "Andres", role: "Manager"}, {name: "Jared", role: "Manager"}, {name: "Smith", role: "Developer"}, {name: "Jared", role: "Marketing"}]
+      events_list = [{name: "Test 1", capacity: 3}, {name: "Test 2", capacity: 1}] #Sorted by size
+      attendance_list = [[], []]
+      role_buckets = {}
+
+      #If there are not preferences
+      employee_list.each do |e|
+        role = e[:role]
+        if !role_buckets.has_key? role
+          role_buckets[role] = Array.new
+        end
+
+        role_buckets[role] << e
+      end
+      # Shuffle each bucket
+      role_buckets.each do |key, value|
+        value.shuffle!
+      end
+
+      queue = Array.new
+      # Distribute equally to fill up the spots by making a custom flatten
+      done = false
+      begin
+        role_buckets.keys.each do |key|
+          list = role_buckets[key]
+          if !list.empty?
+            queue << list.take(1) # Takes the first element
+            role_buckets[key] = list.drop(1) # Eliminates the first element
+          end
+        end
+
+        role_buckets.keep_if {|key, value| !value.empty?}
+
+      end while !role_buckets.empty?
+      queue.flatten!
+
+      # Now that all the employess were distributed equally
+      # Shuffle the events and place them
+      begin
+        events_list.each_with_index do |event, index|
+          puts "Adding to #{event[:name]}"
+          puts "Count: #{attendance_list[index].count}/#{event[:capacity]}"
+          if event[:capacity] > attendance_list[index].count
+            puts "Have capacity for 1 person"
+            attendance_list[index] << queue.take(1)
+            queue = queue.drop(1)
+          end
+        end
+      end while !queue.empty?
+
+      puts attendance_list
+
+    end
 end
