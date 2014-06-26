8bd527d659e2bc046a4f2b55964f99fe7ab4dd8d
Jared Smith <jared@zenpayroll.com>
2014-06-25 22:22:04 -0700

    Add set_groups action, refactor #confirm

--- app/controllers/dispatch_controller.rb
+++ app/controllers/dispatch_controller.rb
@@ -1,49 +1,72 @@
 class DispatchController < ApplicationController
+  protect_from_forgery except: ["confirm"]
+
+  def confirm
+    get_employee
+
+    events = @employee.current_trip.events ### <- this is not working yet
+    selection = params['Body'].to_i
+
+    if selection.zero?
+      @employee.not_attending!(@employee.current_trip)
+    elsif selection.to_i <= events.size
+      preferred_event = events[selection]
+      @employee.prefer(preferred_event)
+    end
+
+    @employee.respond_to(@employee.current_trip)
+  end
 
   def notify
     events = []
     field_trip = FieldTrip.find(params[:field_trip_id])
-    client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN'])
-    
+
     events << "0. I'm Staying in"
     field_trip.events.each_with_index do |event, index|
       events << "#{index+1}. #{event.name}"
     end
     events = events.join("\n")
+    message = "Today's options (reply with number of your preference)\n #{events}"
 
-    field_trip.employees.each do |employee|
-      client.account.sms.messages.create(
-        from: ENV['TWILIO_FROM'],
-        to:   employee.phone,
-        body: "Today's options (reply with number of your preference)\n #{events}")
+    field_trip.employees.each do |trip_employee|
+      send_text(trip_employee.phone, message)
     end
+
     redirect_to field_trip_path(field_trip)
   end
 
-  def confirm
-    get_employee
-    puts "*"*10000
-    client = Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN'])
-    client.account.sms.messages.create(
-      from: ENV['TWILIO_FROM'],
-      to:   "3152378524",
-      body: "Confirmation recieved from someone's phone!")
+  def set_groups
+    field_trip = FieldTrip.find(params[:field_trip_id])
 
-    events = @employee.current_trip.events
-    body = params['Body']
-    if body =~ /0.{0,1}/
-      @employee.not_attending!(@employee.current_trip)
-    elsif body.to_i < events.size
-      @employee.prefer(events[body.to_i])
+    field_trip.set_groups!
+
+    field_trip.groups.each do |group|
+      group.employees.each do |employee|
+        other_employees = group.employees.select {|e| e.name != employee.name}.map(&:name)
+        message = "You are going to #{group.event.name} with: #{other_employees.join('\n')}"
+        send_text(employee.phone, message)
+      end
     end
+
+    redirect_to field_trip_path(field_trip)
   end
+
 
   private
 
   def get_employee
-    unless @employee = Employee.find_by(phone: params['From'])
+    from = params['From'].gsub("+1","")
+    unless @employee = Employee.find_by(phone: from)
       head :not_found
     end
+  end
+
+  def send_text(phone, message)
+    @client ||= Twilio::REST::Client.new(ENV['TWILIO_SID'], ENV['TWILIO_TOKEN'])
+    @client.account.sms.messages.create(
+      from: ENV['TWILIO_FROM'],
+      to:   phone,
+      body: message)
   end
 
 end
